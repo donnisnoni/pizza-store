3de40b9d6b1ee030bab5e4eb53bb41b5390a35e6
Don Alfons Nisnoni <don@kronosinvestments.net>
2022-04-25 15:32:32 +0800

    Add price before discount and improve pizza item
    Signed-off-by: Don Alfons Nisnoni <don@kronosinvestments.net>

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -4,6 +4,15 @@
 
 <div class="pizza-item">
   <img src="/images/{item.img}" alt={item.name} />
-  <h3 class="pizza-name text-center">{item.name}</h3>
-  <div class="pizza-price text-center">${item.price}</div>
+  <div class="pizza-name-and-price-wrapper">
+    <h3 class="pizza-name">{item.name}</h3>
+    <div class="pizza-prices">
+      {#if item.discountPrice}
+        <div class="pizza-price-before">${item.price}</div>
+      {/if}
+      <div class="pizza-price">
+        ${item.discountPrice ? item.discountPrice : item.price}
+      </div>
+    </div>
+  </div>
 </div>
