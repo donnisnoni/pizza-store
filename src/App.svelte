14eaeccea9403f2188c589b1218090785ef8cfd6
Don Alfons Nisnoni <don@kronosinvestments.net>
2022-04-25 15:05:02 +0800

    Initial render pizza items
    Signed-off-by: Don Alfons Nisnoni <don@kronosinvestments.net>

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,40 @@
+<script>
+  import Item from "./components/Item.svelte";
+
+  let pizzas = [
+    {
+      name: "American Classix Cheeseburger",
+      discountPrice: 8,
+      price: 10,
+      img: "american-classic-cheeseburger-550x550px.png",
+    },
+    {
+      name: "Grilled Beef Supreme",
+      discountPrice: 0,
+      price: 12,
+      img: "grill-beef-supreme-550x550px.png",
+    },
+    {
+      name: "Cheesy Meatball Blast",
+      discountPrice: 0,
+      price: 15,
+      img: "meatballpizzathumbnail.png",
+    },
+  ];
+</script>
+
 <heading class="main-heading">
   <h1>Pizza Store</h1>
 </heading>
 
 <div class="pizza-store-main">
-  <div class="pizza-list">
+  <div class="pizza-list-container">
     <h2>Pizza List</h2>
+    <div class="pizza-list">
+      {#each pizzas as pizza}
+        <Item item={pizza} />
+      {/each}
+    </div>
   </div>
 
   <div class="cart">
---
+++ src/components/Item.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let item;
+</script>
+
+<div class="pizza-item">
+  <img src="/images/{item.img}" alt={item.name} />
+  <h3 class="pizza-name text-center">{item.name}</h3>
+  <div class="pizza-price text-center">${item.price}</div>
+</div>
