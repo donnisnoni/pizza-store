6e6953135aa4e860270e99355d04757f5af6c997
Don Alfons Nisnoni <don@kronosinvestments.net>
2022-04-25 14:23:56 +0800

    Main layout done
    Signed-off-by: Don Alfons Nisnoni <don@kronosinvestments.net>

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,13 @@
-<p>Test</p>
+<heading class="main-heading">
+  <h1>Pizza Store</h1>
+</heading>
+
+<div class="pizza-store-main">
+  <div class="pizza-list">
+    <h2>Pizza List</h2>
+  </div>
+
+  <div class="cart">
+    <h2>Cart</h2>
+  </div>
+</div>
