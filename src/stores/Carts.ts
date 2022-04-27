6e7ba37d8c0496c38faca2ef7909aa9bb2bbeee5
Don Alfons Nisnoni <don@kronosinvestments.net>
2022-04-27 17:40:26 +0800

    Save data to local storage
    Signed-off-by: Don Alfons Nisnoni <don@kronosinvestments.net>

--- src/stores/Carts.ts
+++ src/stores/Carts.ts
@@ -1,3 +1,11 @@
 import { writable } from "svelte/store";
 
-export const carts = writable([]);
+const lsNamespace = 'carts'
+
+if (!localStorage.getItem(lsNamespace)) {
+    localStorage.setItem(lsNamespace, JSON.stringify([]))
+}
+
+export const carts = writable(JSON.parse(localStorage.getItem(lsNamespace)))
+
+carts.subscribe((theCarts) => localStorage.setItem(lsNamespace, JSON.stringify(theCarts)))
