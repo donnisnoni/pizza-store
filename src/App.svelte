59f0f6b8b8b624efea1b469396220975ebd1b8c4
donnisnoni <donalfonsnnisnoni@gmail.com>
2022-05-04 14:22:03 +0800

    Separate pizza data to its own file
    Signed-off-by: donnisnoni <donalfonsnnisnoni@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -3,53 +3,9 @@
   import CartItem from "./components/CartItem.svelte";
   import SelectTopingsModal from "./components/SelectTopingsModal.svelte";
   import { carts } from "./stores/Carts";
+  import pizzas from "./constants/Pizzas";
 
   let selectTopingModal;
-
-  let pizzas = [
-    {
-      name: "American Classix Cheeseburger",
-      discountPrice: 8,
-      price: 10,
-      img: "american-classic-cheeseburger-550x550px.png",
-    },
-    {
-      name: "Grilled Beef Supreme",
-      discountPrice: 0,
-      price: 12,
-      img: "grill-beef-supreme-550x550px.png",
-    },
-    {
-      name: "Cheesy Meatball Blast",
-      discountPrice: 0,
-      price: 15,
-      img: "meatballpizzathumbnail.png",
-    },
-    {
-      name: "Arabic Beef Kebab",
-      discountPrice: 0,
-      price: 3,
-      img: "beef_kebab_side.png",
-    },
-    {
-      name: "Ultimate Cheese Melt",
-      discountPrice: 0,
-      price: 6,
-      img: "ultimatecheesemeltnew.png",
-    },
-    {
-      name: "Chicken Dominator",
-      discountPrice: 0,
-      price: 5,
-      img: "chicken-dominator-550x550px.png",
-    },
-    {
-      name: "American All Star",
-      discountPrice: 0,
-      price: 2,
-      img: "american-allstar-550x550px.png",
-    },
-  ];
 
   let currentSelectedPizza = null;
 
