4958c162c24c94ee24b7b4a82b9175b1202f40e8
donnisnoni <donalfonsnnisnoni@gmail.com>
2022-05-06 14:35:43 +0800

    Improve dialog component
    Signed-off-by: donnisnoni <donalfonsnnisnoni@gmail.com>

--- src/components/PizzaDialog.svelte
+++ src/components/PizzaDialog.svelte
@@ -54,54 +54,58 @@
     amount;
 </script>
 
-<dialog class="pizza-dialog" bind:this={dialog} on:close={onClose}>
-  <h2>{pizza.name}</h2>
+<dialog class="dialog-wrapper" bind:this={dialog} on:close={onClose}>
+  <div class="dialog">
+    <h2>{pizza.name}</h2>
 
-  <img
-    class="pizza-img"
-    src="./images/{pizza.img}"
-    alt={pizza.name}
-    title={pizza.name}
-  />
+    <div class="dialog-body">
+      <img
+        class="pizza-img"
+        src="./images/{pizza.img}"
+        alt={pizza.name}
+        title={pizza.name}
+      />
 
-  <div class="pizza-prices">
-    {#if pizza.discountPrice}
-      <span class="pizza-price-before">
-        ${pizzaPriceBefore.toFixed(1)}
-      </span>
-    {/if}
-    <div class="pizza-price">
-      ${pizzaRealPrice.toFixed(1)}
-    </div>
-  </div>
-
-  <h3 class="mt-1">Select topings</h3>
-  <div class="toping-list">
-    {#each topings as toping, index}
-      <div class="toping-list-item">
-        <label>
-          <input
-            type="checkbox"
-            name="topings"
-            bind:group={selectedTopingsId}
-            value={toping.id}
-          />
-          <span class="toping-list-item-name">
-            {toping.label} (${toping.price})
+      <div class="pizza-prices">
+        {#if pizza.discountPrice}
+          <span class="pizza-price-before">
+            ${pizzaPriceBefore.toFixed(1)}
           </span>
-        </label>
+        {/if}
+        <div class="pizza-price">
+          ${pizzaRealPrice.toFixed(1)}
+        </div>
       </div>
-    {/each}
-  </div>
 
-  <h3 class="mt-1">Amount</h3>
+      <h3 class="mt-1">Select topings</h3>
+      <div class="toping-list">
+        {#each topings as toping, index}
+          <div class="toping-list-item">
+            <label>
+              <input
+                type="checkbox"
+                name="topings"
+                bind:group={selectedTopingsId}
+                value={toping.id}
+              />
+              <span class="toping-list-item-name">
+                {toping.label} (${toping.price})
+              </span>
+            </label>
+          </div>
+        {/each}
+      </div>
 
-  <AmountButton bind:amount />
+      <h3 class="mt-1">Amount</h3>
 
-  <div class="modal-actions">
-    <button on:click={close}>Cancel</button>
-    <button class="add-to-cart-button-on-modal" on:click={addToCart}>
-      Add to cart
-    </button>
+      <AmountButton bind:amount />
+    </div>
+
+    <div class="modal-actions">
+      <button on:click={close}>Cancel</button>
+      <button class="add-to-cart-button-on-modal" on:click={addToCart}>
+        Add to cart
+      </button>
+    </div>
   </div>
 </dialog>
